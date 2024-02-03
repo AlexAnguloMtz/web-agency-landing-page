a72aaa7e1f1e36bf698d44f4bb700755aa2c41fb
alex <aangulo@geniuscode.mx>
2024-02-03 15:16:47 -0700

    Import hamburguer in nav component

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavLinkComponent } from './nav-link/nav-link.component';
+import { HamburguerButtonComponent } from './hamburguer-button/hamburguer-button.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [NavLinkComponent],
+  imports: [NavLinkComponent, HamburguerButtonComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.sass',
 })
