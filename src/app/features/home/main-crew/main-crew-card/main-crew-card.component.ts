01445bcc8893ef07575fc20efdb79f6395a5cab1
alex <aangulo@geniuscode.mx>
2024-02-04 13:30:07 -0700

    Implement getSocialNetworks() at MainCrewCardComponent

--- src/app/features/home/main-crew/main-crew-card/main-crew-card.component.ts
+++ src/app/features/home/main-crew/main-crew-card/main-crew-card.component.ts
@@ -13,8 +13,33 @@ export class MainCrewCardComponent {
   @Input() profilePicture: string;
   @Input() roleDescription: string;
   @Input() gender: string;
+  @Input() twitterHref?: string;
+  @Input() githubHref?: string;
+  @Input() linkedInHref?: string;
 
   getOverlayImage(): string {
     return `url(assets/images/placeholders/member-secret-${this.gender}.png`;
   }
+
+  getSocialNetworks(): Array<SocialNetwork> {
+    const twitterImage = '';
+    const githubImage = '';
+    const linkedInImage = '';
+    const socialNetworks: Array<SocialNetwork> = [];
+    if (this.twitterHref) {
+      socialNetworks.push({ image: twitterImage, href: this.twitterHref });
+    }
+    if (this.githubHref) {
+      socialNetworks.push({ image: githubImage, href: this.githubHref });
+    }
+    if (this.linkedInHref) {
+      socialNetworks.push({ image: linkedInImage, href: this.linkedInHref });
+    }
+    return socialNetworks;
+  }
 }
+
+type SocialNetwork = {
+  image: string;
+  href: string;
+};
