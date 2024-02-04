6c29a42f036df2a92efe65dce28c3562eef401e5
alex <aangulo@geniuscode.mx>
2024-02-04 16:00:17 -0700

    Add crew header

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavComponent } from '../../components/nav/nav.component';
 import { MainCrewComponent } from './main-crew/main-crew.component';
 import { HeroComponent } from './hero/hero.component';
+import { CrewComponent } from './crew/crew.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavComponent, HeroComponent, MainCrewComponent],
+  imports: [NavComponent, HeroComponent, MainCrewComponent, CrewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass',
 })
