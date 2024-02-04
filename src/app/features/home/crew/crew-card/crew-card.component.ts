21f08d016159fdc5058b0722080e7910ffe5d0f7
alex <aangulo@geniuscode.mx>
2024-02-04 16:14:03 -0700

    Generate crew card component

---
+++ src/app/features/home/crew/crew-card/crew-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crew-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './crew-card.component.html',
+  styleUrl: './crew-card.component.sass'
+})
+export class CrewCardComponent {
+
+}
