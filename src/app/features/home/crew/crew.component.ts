e17a4e00a36043b7ec9991c2df458ab3e23268f2
alex <aangulo@geniuscode.mx>
2024-02-04 16:01:24 -0700

    Copy placeholder members to crew comp

--- src/app/features/home/crew/crew.component.ts
+++ src/app/features/home/crew/crew.component.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import { CrewMember } from '../../../types/CrewMember';
+
+const baseImageUrl = '../../../../assets/images/placeholders/';
 
 @Component({
   selector: 'app-crew',
   standalone: true,
   imports: [],
   templateUrl: './crew.component.html',
-  styleUrl: './crew.component.sass'
+  styleUrl: './crew.component.sass',
 })
 export class CrewComponent {
-
+  members: Array<CrewMember> = [
+    {
+      name: 'Marco Forest',
+      gender: 'male',
+      githubUrl: 'http://www.github.com',
+      linkedInUrl: 'http://www.linkedin.com',
+      twitterUrl: 'http://www.twitter.com',
+      role: {
+        name: 'CTO',
+        description: 'Manages all technology related stuff',
+      },
+      phrase:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+      profilePicture: baseImageUrl + 'placeholder-anime-CTO.jpg',
+    },
+    {
+      name: 'John Smith',
+      gender: 'male',
+      githubUrl: 'http://www.github.com',
+      linkedInUrl: 'http://www.linkedin.com',
+      twitterUrl: 'http://www.twitter.com',
+      role: {
+        name: 'Team Leader',
+        description: 'Fundador y Director Ejecutivo de My Company',
+      },
+      phrase: 'Atrévete a soñar, desafía los límites, crea el futuro',
+      profilePicture: baseImageUrl + 'placeholder-anime-CEO.jpg',
+    },
+    {
+      name: 'Diana Skies',
+      gender: 'female',
+      githubUrl: 'http://www.github.com',
+      linkedInUrl: 'http://www.linkedin.com',
+      twitterUrl: 'http://www.twitter.com',
+      role: {
+        name: 'CFO',
+        description: 'Manages all money related stuff',
+      },
+      phrase:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+      profilePicture: baseImageUrl + 'placeholder-anime-CFO.png',
+    },
+  ];
 }
