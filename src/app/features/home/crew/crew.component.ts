930cf2b5d1b48f15b435a328097e4ca2a693b7d2
alex <aangulo@geniuscode.mx>
2024-02-04 15:02:10 -0700

    Generate app-crew component

---
+++ src/app/features/home/crew/crew.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crew',
+  standalone: true,
+  imports: [],
+  templateUrl: './crew.component.html',
+  styleUrl: './crew.component.sass'
+})
+export class CrewComponent {
+
+}
