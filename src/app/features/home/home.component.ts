76c67fa8f983657028b6da90a28d853f5ee7c83d
alex <aangulo@geniuscode.mx>
2024-02-04 20:33:37 -0700

    Import AboutUsComponent in HomeComponent

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -3,11 +3,18 @@ import { NavComponent } from '../../components/nav/nav.component';
 import { MainCrewComponent } from './main-crew/main-crew.component';
 import { HeroComponent } from './hero/hero.component';
 import { CrewComponent } from './crew/crew.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavComponent, HeroComponent, MainCrewComponent, CrewComponent],
+  imports: [
+    NavComponent,
+    HeroComponent,
+    MainCrewComponent,
+    CrewComponent,
+    AboutUsComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass',
 })
