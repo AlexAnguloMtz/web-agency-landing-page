ad0efe0ca32a94f724dcd3949b548faaaf1f7c3c
alex <aangulo@geniuscode.mx>
2024-02-04 20:15:30 -0700

    Add social network links to crew cards

--- src/app/features/home/crew/crew-card/crew-card.component.ts
+++ src/app/features/home/crew/crew-card/crew-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { SocialNetwork } from '../../../../types/SocialNetwork';
 
 @Component({
   selector: 'app-crew-card',
@@ -11,4 +12,34 @@ export class CrewCardComponent {
   @Input() name: string;
   @Input() image: string;
   @Input() role: string;
+  @Input() githubUrl?: string;
+  @Input() linkedInUrl?: string;
+  @Input() twitterUrl?: string;
+
+  getSocialNetworks(): Array<SocialNetwork> {
+    const logoBaseUrl: string = 'assets/images/logos';
+    const socialNetworks: Array<SocialNetwork> = [];
+    if (this.githubUrl) {
+      socialNetworks.push({
+        imageSrc: `${logoBaseUrl}/github-logo.png`,
+        imageAlt: `${this.name} github account`,
+        url: this.githubUrl,
+      });
+    }
+    if (this.linkedInUrl) {
+      socialNetworks.push({
+        imageSrc: `${logoBaseUrl}/linkedin-logo.png`,
+        imageAlt: `${this.name} linkedin account`,
+        url: this.linkedInUrl,
+      });
+    }
+    if (this.twitterUrl) {
+      socialNetworks.push({
+        imageSrc: `${logoBaseUrl}/x-logo.png`,
+        imageAlt: `${this.name} twitter account`,
+        url: this.twitterUrl,
+      });
+    }
+    return socialNetworks;
+  }
 }
